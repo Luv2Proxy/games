50320ed6859ecb9ab778aa1380ed0785de1d8809
Luv2Proxy <abohra1@outlook.com>
2025-02-27 17:57:36 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -22,7 +22,7 @@ const games = [
 ];
 
 // Path to the games folder
-const gamesPath = '/games/alu-games-main/';
+const gamesPath = '/alu-games-main/';
 
 // Load the game list
 function loadGameList() {
